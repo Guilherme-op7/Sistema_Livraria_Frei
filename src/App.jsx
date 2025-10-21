8c728e9f6d86dc793dd133d24a06129343d3c110
Guilherme-op7 <netoguilherme989@gmail.com>
2025-10-21 17:32:31 -0300

    testando tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-blue-800 h-dvh flex items-center justify-center">
+      <h1 className="bg-blue-200 p-4 rounded-sm font-medium text-xl">testando tailwind</h1>
+    </div>
+  )
+}
+
+export default App
